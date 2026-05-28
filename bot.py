641f33c0dbb3276a81a2e34cca4bc45edac4fdbf
shifrakode9-cell <shifrakode9@gmail.com>
2026-05-28 04:48:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from highrise import BaseBot
+from highrise import BaseBot, User
 
 class MyBot(BaseBot):
     async def on_start(self, session_metadata):
         print("--- السيرفر متصل بنجاح! ---")
-        await self.highrise.chat("تم الاتصال! البوت جاهز.")
+        await self.highrise.chat("تم الاتصال! البوت يعمل الآن.")
 
-    async def on_chat(self, user, message):
+    async def on_chat(self, user: User, message: str) -> None:
         if message == "مشاركة":
             await self.highrise.chat(f"أهلاً {user.username}، تم تسجيلك.")
